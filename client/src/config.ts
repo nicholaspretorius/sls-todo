90b7215f7946cddb220d6656c1d9f19b096897bd
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-03-27 14:12:38 +0200

    chore: Update config per URL and Auth0

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
+const apiId = 'xnw0i26rzi'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'nicholaspre.eu.auth0.com',            // Auth0 domain
+  clientId: '69dRcEmdGjElhxz9aEtEobvCi6z8x4RX',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }

c6b537c2a680039004d3ddb9a5ae2db72e29896d
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-03-31 15:48:55 +0200

    feat: Add function to create certificate

--- backend/src/auth/utils.ts
+++ backend/src/auth/utils.ts
@@ -21,3 +21,10 @@ export function parseUserId(jwtToken: string): string {
 
   return decodedJwt.sub
 }
+
+// https://github.com/sgmeyer/auth0-node-jwks-rs256/blob/master/src/lib/utils.js
+export function certToPEM(cert: string) {
+  cert = cert.match(/.{1,64}/g).join('\n');
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+  return cert;
+}

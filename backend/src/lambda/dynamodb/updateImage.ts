4d76b6591f90a109c015b3ee23e6e3fe040f3f4b
Nicholas Pretorius <nicholaspretorius@gmail.com>
2020-03-31 12:54:52 +0200

    feat: New handlers for upload, notifications

---
+++ backend/src/lambda/dynamodb/updateImage.ts
@@ -0,0 +1,56 @@
+import { SNSEvent, SNSHandler, S3Event, S3EventRecord } from "aws-lambda";
+import "source-map-support";
+// import * as AWS from "aws-sdk";
+
+import { createLogger } from "./../../utils/logger";
+import { updateAttachmentUrl } from "./../../businessLogic/todos";
+import { getTodoIdByImageId } from "./../../businessLogic/todoImages";
+
+const logger = createLogger("updateImage");
+
+// const s3 = new AWS.S3();
+
+// const imagesBucket = process.env.TODO_IMAGES_S3_BUCKET;
+
+export const handler: SNSHandler = async (event: SNSEvent) => {
+    logger.info("Processing SNS Event: ", { event });
+
+    for (const snsRecord of event.Records) {
+        logger.info("Processing SNS Record: ", { snsRecord });
+
+        const s3EventStr = snsRecord.Sns.Message;
+        logger.info("Processing SNS Message: ", { s3EventStr });
+
+        const s3Event: S3Event = JSON.parse(s3EventStr);
+
+        for (const record of s3Event.Records) {
+            await updateImage(record);
+        }
+
+    }
+}
+
+async function updateImage(record: S3EventRecord) {
+    logger.info("Record: ", { record });
+    const key = record.s3.object.key;
+
+    const url = `https://sls-todo-images-bucket-dev.s3.amazonaws.com/${key}`;
+
+    const image = await getTodoIdByImageId(key);
+    logger.info("Image: ", { image });
+
+    try {
+        await updateAttachmentUrl(image.todoId, url);
+    } catch (e) {
+        logger.error("Error updating attachmentUrl: ", { error: e });
+    }
+
+    // const response = await s3.getObject({
+    //     Bucket: imagesBucket,
+    //     Key: key
+    // }).promise();
+
+    // logger.info("Response: ", { response });
+
+    // const body = response.Body;
+}
